ca85a0f58ebaf5791bf5d898701d2a140a36a90c
drA666 <43187328+drA666@users.noreply.github.com>
2021-05-19 00:01:27 +0300

    lab2

---
+++ lab2/src/components/Diagram.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { PieChart } from 'react-native-svg-charts';
+
+export const Diagram = () => {
+  const data = [
+    {
+      key: 1,
+      value: 5,
+      svg: { fill: 'brown' },
+    },
+    {
+      key: 2,
+      value: 5,
+      svg: { fill: 'rgb(116,204,244)' },
+    },
+    {
+      key: 3,
+      value: 10,
+      svg: { fill: 'orange' },
+    },
+    {
+      key: 4,
+      value: 80,
+      svg: { fill: 'blue' },
+    },
+  ];
+
+  return <PieChart data={data} innerRadius={'50%'} style={{
+    height: 320,
+    width: 400,
+    padding: 10,
+    marginTop: 10,
+  }}></PieChart>
+};
